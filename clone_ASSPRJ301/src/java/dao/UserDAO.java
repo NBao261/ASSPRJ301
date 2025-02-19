677976490a5ee7e508762a8496f6e7e5bf25d311
NBao261 <ncb2601@gmail.com>
2025-02-19 16:59:11 +0700

    Update UserDAO.java

--- clone_ASSPRJ301/src/java/dao/UserDAO.java
+++ clone_ASSPRJ301/src/java/dao/UserDAO.java
@@ -12,23 +12,20 @@ import java.util.logging.Logger;
 import utils.DBUtils;
 
 public class UserDAO implements IDAO<UserDTO, String> {
-
+    
     @Override
     public boolean create(UserDTO entity) {
-        String sql = "INSERT [dbo].[tblUsers] ([userID], [fullName], [roleID], [password]) "
-                + "VALUES (?, ?, ?, ?)";
-        try {
-            Connection conn = DBUtils.getConnection();
-            PreparedStatement ps = conn.prepareCall(sql);
+        String sql = "INSERT INTO [tblUsers] ([userID], [fullName], [roleID], [password]) VALUES (?, ?, ?, ?)";
+        try (Connection conn = DBUtils.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+             
             ps.setString(1, entity.getUserID());
             ps.setString(2, entity.getFullName());
             ps.setString(3, entity.getRoleID());
-            ps.setString(4, entity.getPassword());
-            int n = ps.executeUpdate();
-            return n > 0;
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
+            ps.setString(4, entity.getPassword()); // Cần mã hóa password trước khi lưu
+
+            return ps.executeUpdate() > 0;
+        } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return false;
@@ -37,22 +34,20 @@ public class UserDAO implements IDAO<UserDTO, String> {
     @Override
     public List<UserDTO> readAll() {
         List<UserDTO> list = new ArrayList<>();
-        String sql = "SELECT * FROM [tblUsers];";
-        try {
-            Connection conn = DBUtils.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
+        String sql = "SELECT [userID], [fullName], [roleID] FROM [tblUsers]"; // Chỉ lấy cột cần thiết
+        try (Connection conn = DBUtils.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
             while (rs.next()) {
-                UserDTO user = new UserDTO(
-                        rs.getString("userID"),
-                        rs.getString("fullName"),
-                        rs.getString("roleID"),
-                        rs.getString("password"));
-                list.add(user);
+                list.add(new UserDTO(
+                    rs.getString("userID"),
+                    rs.getString("fullName"),
+                    rs.getString("roleID"),
+                    null  // Không lấy password để đảm bảo bảo mật
+                ));
             }
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
+        } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return list;
@@ -60,23 +55,21 @@ public class UserDAO implements IDAO<UserDTO, String> {
 
     @Override
     public UserDTO readById(String id) {
-        String sql = "SELECT * FROM tblUsers WHERE userID = ?";
-        try {
-            Connection conn = DBUtils.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
+        String sql = "SELECT [userID], [fullName], [roleID], [password] FROM [tblUsers] WHERE userID = ? ";
+        try (Connection conn = DBUtils.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, id);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                UserDTO user = new UserDTO(
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new UserDTO(
                         rs.getString("userID"),
                         rs.getString("fullName"),
                         rs.getString("roleID"),
-                        rs.getString("password"));
-                return user;
+                        rs.getString("password")
+                    );
+                }
             }
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
+        } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
@@ -84,24 +77,17 @@ public class UserDAO implements IDAO<UserDTO, String> {
 
     @Override
     public boolean update(UserDTO entity) {
-        String sql = "UPDATE [tblUsers] SET "
-                + "[fullName] = ?, "
-                + "[roleID] = ?"
-                + "[password] = ?"
-                + "WHERE [userID] = ?";
+        String sql = "UPDATE [tblUsers] SET [fullName] = ?, [roleID] = ?, [password] = ? WHERE [userID] = ?";
+        try (Connection conn = DBUtils.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
 
-        try {
-            Connection conn = DBUtils.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, entity.getFullName());
             ps.setString(2, entity.getRoleID());
             ps.setString(3, entity.getPassword());
             ps.setString(4, entity.getUserID());
-            int n = ps.executeUpdate();
-            return n > 0;
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
+
+            return ps.executeUpdate() > 0;
+        } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return false;
@@ -109,17 +95,13 @@ public class UserDAO implements IDAO<UserDTO, String> {
 
     @Override
     public boolean delete(String id) {
-        String sql = "DELETE FROM [tblUsers] "
-                + "WHERE [userID] = ?";
-        try {
-            Connection conn = DBUtils.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
+        String sql = "DELETE FROM [tblUsers] WHERE [userID] = ?";
+        try (Connection conn = DBUtils.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
             ps.setString(1, id);
-            int n = ps.executeUpdate();
-            return n > 0;
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
+            return ps.executeUpdate() > 0;
+        } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return false;
@@ -128,28 +110,24 @@ public class UserDAO implements IDAO<UserDTO, String> {
     @Override
     public List<UserDTO> search(String searchTerm) {
         List<UserDTO> list = new ArrayList<>();
-        String sql = "SELECT [userID], [fullName], [roleID], [password] FROM [tblUsers] "
-                + "WHERE [userID] LIKE ?"
-                + "OR [fullName] LIKE ?"
-                + "OR [roleID] LIKE ?";
-        try {
-            Connection conn = DBUtils.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
+        String sql = "SELECT [userID], [fullName], [roleID] FROM [tblUsers] WHERE [userID] LIKE ? OR [fullName] LIKE ? OR [roleID] LIKE ?";
+        
+        try (Connection conn = DBUtils.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
             String searchPattern = "%" + searchTerm + "%";
-            //Thiet lap gia tri cho tat ca cac tham so
             ps.setString(1, searchPattern);
             ps.setString(2, searchPattern);
             ps.setString(3, searchPattern);
-            
+
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
-                    UserDTO user = new UserDTO(
-                            rs.getString("userID"),
-                            rs.getString("fullName"),
-                            rs.getString("roleID"),
-                            rs.getString("password")
-                    );
-                    list.add(user);
+                    list.add(new UserDTO(
+                        rs.getString("userID"),
+                        rs.getString("fullName"),
+                        rs.getString("roleID"),
+                        null // Không lấy password
+                    ));
                 }
             }
         } catch (ClassNotFoundException | SQLException ex) {
@@ -158,3 +136,4 @@ public class UserDAO implements IDAO<UserDTO, String> {
         return list;
     }
 }
+
